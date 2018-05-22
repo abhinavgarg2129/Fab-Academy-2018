4fc7e3515e2e0e81232af772fc4da7f7e7e1cb1f
Abhinav Garg <35521837+abhinavgarg2129@users.noreply.github.com>
2018-05-22 20:02:46 +0530

    Add files via upload

---
+++ blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // initialize digital pin 8 as an output. default will be 13 in the arduino example.
+  pinMode(3, OUTPUT);
+ }
+
+// the loop function runs over and over again forever
+
+void loop() {
+  digitalWrite(3,HIGH);
+  delay(2000);
+digitalWrite(3,LOW);
+delay(2000);
+ }
---
+++ button.ino
@@ -0,0 +1,23 @@
+void setup() {
+   
+// initialize digital pin 8 as an output.
+pinMode(3, OUTPUT);
+pinMode(4,INPUT);
+digitalWrite(4,HIGH); // internal pullup
+
+}
+ 
+// the loop function runs over and over again forever
+void loop() {
+ 
+if (digitalRead(4)){
+digitalWrite(3, LOW);   // turn the LED on (HIGH is the voltage level)
+ 
+}
+else {
+ 
+digitalWrite(3, HIGH);    // turn the LED off by making the voltage LOW
+
+}
+}
+
