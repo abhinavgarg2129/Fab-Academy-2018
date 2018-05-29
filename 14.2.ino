e03894c0b21f18d4b035411a911ce442e7c8891d
Abhinav Garg <35521837+abhinavgarg2129@users.noreply.github.com>
2018-05-29 21:08:22 +0530

    Add files via upload

---
+++ 14.2.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+ const byte rxPin = 2;
+ const byte txPin= 1;
+ SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(3, OUTPUT);
+  pinMode(4,INPUT);
+  digitalWrite(7,HIGH);
+    // define pin modes for tx, rx:
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+//listen to serial port
+if (mySerial.read()=='2')
+{
+  digitalWrite(3,HIGH);
+  
+  }
+ 
+}
